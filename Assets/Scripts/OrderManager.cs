49bb6835d000e72228a01f4c5d3b3fb42eb9366b
Malaika Joiner <malaikajoiner@gmail.com>
2025-12-12 08:44:26 -0800

    added sound effects

--- Assets/Scripts/OrderManager.cs
+++ Assets/Scripts/OrderManager.cs
@@ -1,11 +1,13 @@
 using UnityEngine;
 using System;
+using System.Collections;
 public class OrderManager : MonoBehaviour
 {
     public GameObject[] orderPanels; // Assign in Inspector
     public GameObject PlayerUI; //currentOrder popup inside Player's UI 
     public GameObject OrderTimer; // timermanager(order) inside Player's UI
-
+    public AudioSource source;
+    public AudioClip orderHereSound;
     public string currentOrder;
 
     void Start()
@@ -17,12 +19,21 @@ public class OrderManager : MonoBehaviour
     {
         return currentOrder;
     }
-    
+
+    IEnumerator SetTimer()
+    {
+        System.Random random = new System.Random();
+        float cooldown = random.Next(120);
+        yield return new WaitForSeconds(cooldown);
+        StartNewOrder();
+    }
+
     public void StartNewOrder()
     {
         PlayerUI.SetActive(true); // makes the player's You have an order! UI popup appear
         OrderTimer.GetComponent<timerManagerOrder>().ResetTime(); // resets the timer for popup
-
+        source.PlayOneShot(orderHereSound);
+        
         System.Random random = new System.Random();
         int choice = random.Next(2);
 
@@ -54,7 +65,12 @@ public class OrderManager : MonoBehaviour
 
     public void CompleteOrder()
     {
+        PlayerUI.SetActive(false);
+        foreach (GameObject panel in orderPanels)
+        {
+            panel.SetActive(false); // have the panel gone by defaul
+        }
         Debug.Log("Order completed!");
-        StartNewOrder();
+        StartCoroutine(SetTimer());
     }
 }
