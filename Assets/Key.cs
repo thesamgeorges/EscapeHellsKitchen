38116d8c1896469e9966aadb3dab9102606e6d47
AnaMoraisA <amoraisarantes0749@sdsu.edu>
2025-12-14 22:00:03 -0800

    key comes out

---
+++ Assets/Key.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void showKey()
+    {
+        gameObject.SetActive(true);
+    }
+}
