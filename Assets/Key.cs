a0f0d3d8a89e30ca42de7225f7ace819ae67de83
AnaMoraisA <amoraisarantes0749@sdsu.edu>
2025-12-18 01:42:01 -0800

    comments on key script

--- Assets/Key.cs
+++ Assets/Key.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Key : MonoBehaviour
 {
-    // Start is called before the first frame update
+    //show key when lock combination is correctly inputted
     public void showKey()
     {
         gameObject.SetActive(true);
